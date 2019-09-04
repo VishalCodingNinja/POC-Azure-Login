94bb8ec2020bd245e649344b8c211275509aee29
Vishal Singh <Vishal.Singh@euromonitor.com>
2019-09-04 13:35:35 +0530

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using POC_Azure_Login.Models;
+
+namespace POC_Azure_Login.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            
+        }
+        [AllowAnonymous]//now any one able to see this specific index page
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize("groupname")]//your group name if you are authenticating using group
+        public IActionResult Privacy()
+        {
+            ViewData["Message"] = "your contact page";
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Contact()
+        {
+              var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+
+            //You get the user’s first and last name below:
+            ViewBag.Name = userClaims?.FindFirst("name")?.Value;
+
+            // The 'preferred_username' claim can be used for showing the username
+            ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
+
+            // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
+            ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+            // TenantId is the unique Tenant Id - which represents an organization in Azure AD
+            ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POC_Azure_Login.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
